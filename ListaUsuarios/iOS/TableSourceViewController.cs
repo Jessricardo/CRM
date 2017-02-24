d3ff58f73caf1df6c298f0755f15f732e69c6f81
Paul Gregorio German Millan <g1r3e5g7@gmail.com>
2017-02-24 12:48:14 -0700

    TableView
    Mostrando items de la lista

---
+++ ListaUsuarios/iOS/TableSourceViewController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace ListaUsuarios.iOS
+{
+	public partial class TableSourceViewController : UITableViewSource
+	{
+
+		string[] TableItems;
+		string CellIdentifier = "TableCell";
+		TableViewController owner;
+		public TableSourceViewController(string[] items, TableViewController owner)
+		{
+			TableItems = items;
+			this.owner = owner;
+		}
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+			return TableItems.Length;
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			
+			UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);
+
+			string item = TableItems[indexPath.Row];
+
+			//---- if there are no cells to reuse, create a new one
+			if (cell == null)
+			{ cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier); }
+
+			cell.TextLabel.Text = item;
+			cell.Accessory = UITableViewCellAccessory.DetailDisclosureButton;
+
+			return cell;
+		}
+
+		public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+		{
+			UIAlertController okAlertController = UIAlertController.Create("Fila seleccionada", TableItems[indexPath.Row], UIAlertControllerStyle.Alert);
+			okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+
+			tableView.DeselectRow(indexPath, true);
+			owner.PresentViewController(okAlertController, true, null);
+
+		}
+
+		public override void AccessoryButtonTapped(UITableView tableView, NSIndexPath indexPath)
+		{
+			UIAlertController okAlertController = UIAlertController.Create("Contacto seleccionado", TableItems[indexPath.Row], UIAlertControllerStyle.Alert);
+			okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+			owner.PresentViewController(okAlertController, true, null);
+
+			tableView.DeselectRow(indexPath, true);
+		}
+		public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, Foundation.NSIndexPath indexPath)
+		{
+			switch (editingStyle)
+			{
+				case UITableViewCellEditingStyle.Delete:
+					// remove the item from the underlying data source
+					TableItems.RemoveAt(indexPath.Row);
+					// delete the row from the table
+					tableView.DeleteRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
+					break;
+				case UITableViewCellEditingStyle.None:
+					Console.WriteLine("CommitEditingStyle:None called");
+					break;
+			}
+		}
+		public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
+		{
+			return true; // return false if you wish to disable editing for a specific indexPath or for all rows
+		}
+		public override string TitleForDeleteConfirmation(UITableView tableView, NSIndexPath indexPath)
+		{   // Optional - default text is 'Delete'
+			return "Borrado (" + TableItems[indexPath.Row] + ")";
+		}
+
+	}
+}
+
---
+++ ListaUsuarios/iOS/TableViewController.cs
@@ -0,0 +1,23 @@
+ 	using Foundation;
+using System;
+using UIKit;
+
+namespace ListaUsuarios.iOS
+{
+    public partial class TableViewController : UITableViewController
+    {
+        public TableViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			tvUsuarios = new UITableView(View.Bounds); // defaults to Plain style
+			string[] tableItems = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
+			//tvUsuarios.Source = new TableSourceViewController(tableItems);
+			Add(tvUsuarios);
+			tvUsuarios.Source = new TableSourceViewController(tableItems, this);
+		}
+    }
+}
