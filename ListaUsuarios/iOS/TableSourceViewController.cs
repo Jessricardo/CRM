6fdc9ec1a8d570c548636f97171824537e6c1f4a
Paul Gregorio German Millan <g1r3e5g7@gmail.com>
2017-02-28 08:49:11 -0700

    No vale
    No sirve

--- ListaUsuarios/iOS/TableSourceViewController.cs
+++ ListaUsuarios/iOS/TableSourceViewController.cs
@@ -60,29 +60,35 @@ namespace ListaUsuarios.iOS
 
 			tableView.DeselectRow(indexPath, true);
 		}
-		public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, Foundation.NSIndexPath indexPath)
-		{
-			switch (editingStyle)
-			{
-				case UITableViewCellEditingStyle.Delete:
-					// remove the item from the underlying data source
 
-					// delete the row from the table
-					tableView.DeleteRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
-					break;
-				case UITableViewCellEditingStyle.None:
-					Console.WriteLine("CommitEditingStyle:None called");
-					break;
-			}
-		}
-		public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
-		{
-			return true; // return false if you wish to disable editing for a specific indexPath or for all rows
-		}
-		public override string TitleForDeleteConfirmation(UITableView tableView, NSIndexPath indexPath)
-		{   // Optional - default text is 'Delete'
-			return "Borrado (" + TableItems[indexPath.Row] + ")";
-		}
+
+
+		//borrado con un deslizamiento
+		//public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, Foundation.NSIndexPath indexPath)
+		//{
+		//	switch (editingStyle)
+		//	{
+		//		case UITableViewCellEditingStyle.Delete:
+		//			// remove the item from the underlying data source
+		//			TableItems.RemoveAt(indexPath.Row);
+		//			// delete the row from the table
+		//			tableView.DeleteRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
+		//			break;
+		//		case UITableViewCellEditingStyle.None:
+		//			Console.WriteLine("CommitEditingStyle:None called");
+		//			break;
+		//	}
+		//}
+		//public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
+		//{
+		//	return true; // return false if you wish to disable editing for a specific indexPath or for all rows
+		//}
+		//public override string TitleForDeleteConfirmation(UITableView tableView, NSIndexPath indexPath)
+		//{   // Optional - default text is 'Delete'
+		//	return "Borrado (" + TableItems[indexPath.Row] + ")";
+		//}
+
+
 
 	}
 }
