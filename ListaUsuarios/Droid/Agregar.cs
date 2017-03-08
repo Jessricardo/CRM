d3c5dcc81ad71bfd15edfbbf72520a8739022671
Paul Gregorio German Millan <g1r3e5g7@gmail.com>
2017-03-08 12:52:11 -0700

    Ya esta agregar, falta el boton c:

--- ListaUsuarios/Droid/Agregar.cs
+++ ListaUsuarios/Droid/Agregar.cs
@@ -20,6 +20,7 @@ namespace ListaUsuarios.Droid
         Button btnGuardar;
         IContactRepository db;
         Contact contacto;
+		private EditText edtNombre, edtDomicilio, edtCorreo, edtTelefono, edtPais, edtEstado; 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
@@ -27,12 +28,21 @@ namespace ListaUsuarios.Droid
             string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "dbCRM.db3");
             db = new SQLiteContactRepository(dbPath);
             contacto = new Contact();
+
+			edtNombre = FindViewById<EditText>(Resource.Id.edtNombre);
+			edtPais = FindViewById<EditText>(Resource.Id.edtPais);
+			edtDomicilio = FindViewById<EditText>(Resource.Id.edtDomicilio);
+			edtCorreo = FindViewById<EditText>(Resource.Id.edtEmail);
+			edtTelefono = FindViewById<EditText>(Resource.Id.edtTelefono);
+			edtEstado = FindViewById<EditText>(Resource.Id.edtEstado);
+
             btnGuardar = FindViewById<Button>(Resource.Id.btnGuardar);
             btnGuardar.Click += Guardar;
         }
 
         private void Guardar(object sender, EventArgs e)
         {
+			contacto.contactName = edtNombre.Text;
                 db.Crear(contacto);
                 Toast.MakeText(this, "¡Guardado!", ToastLength.Long).Show();
                 StartActivity(typeof(MainActivity));
